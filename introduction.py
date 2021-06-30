0eb99f6fe2558e93704d5f8187d0b2fa40afbedb
offenanil <offenanil@gmail.com>
2021-06-30 11:54:39 +1000

    test project

---
+++ introduction.py
@@ -0,0 +1,14 @@
+# # # # a =
+# # # # print(type(a))
+# # # # print(dir(1))
+# # # a = 5
+# # # ouput(a)
+# # a = input("enter a")
+# # b = input(" entere b")
+# # print(int(a+b))
+# #
+# FName = "Anil"
+# SName = "Acharya"
+# print( FName +'      '+ SName)
+x = input("")
+output(x)
